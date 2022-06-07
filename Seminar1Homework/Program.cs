8a8ad521423a983c6901cc3e679b30e8d7739984
Valeria <piatka2010@gmail.com>
2022-06-07 20:42:17 +0300

    homework1 added

---
+++ Seminar1Homework/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+int num1, num2, max;
+
+Console.Write("Input num1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input num2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if(num1 > num2)
+{
+    Console.WriteLine("max = num1");
+}
+else
+{
+    Console.WriteLine("max = num2");
+} 
+*/
+
+/*
+int a, b, c, max;
+
+Console.Write("Input a: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input c: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
+
+*/
+
+/*
+int a;
+
+Console.Write("Input a: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("чёт");
+}
+else
+{
+    Console.WriteLine("нечет");
+}
+*/
+
+/*
+int num;
+
+Console.Write ("Input num: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = 0;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}
+
+*/
+
+
