cfa2289f8d861f81e735fb0646aa73e9dd8541e6
Valeria <piatka2010@gmail.com>
2022-06-17 11:58:23 +0300

    second homework added

---
+++ Seminar2Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿/*int MiddleNumber()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Current number is " + num);
+
+    int result = num % 100 / 10;
+    return result;
+}
+
+Console.WriteLine(MiddleNumber());
+*/
+
+/*
+Console.Write("Input any number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText = Convert.ToString(num);
+
+if(numText.Length > 2)
+{
+    Console.WriteLine("third number is " + numText [2]);
+}
+else
+{
+    Console.WriteLine("third number is absent");
+}
+*/
+
+
+  
+   
+/*void WeekDay(string number)
+{
+  Console.WriteLine("Enter a numder of a week day: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+
+  if (num == 6 && num == 7)
+  {
+    Console.WriteLine("This is weekend! Huray!");
+  }
+  else 
+  {
+    Console.WriteLine("This is working day");
+  }
+   
+}
+
+
+
+
+
