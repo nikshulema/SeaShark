9f52754eaf423941fc3cc74e79ef82ccac03f07f
Valeria <piatka2010@gmail.com>
2022-06-19 23:09:57 +0300

    forth homework added

---
+++ Seminar4Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿/*int number;
+int step;
+int count = 1;
+int result = number;
+
+while (count < step)
+{
+    result = result * number;
+    count++;
+}
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter step: ");
+int step = Convert.ToInt32(Console.ReadLine());
+int result = Exponenta(number);
+Console.WriteLine($"Exponenta: {result}");
+*/
+
+
+
+
+/*int [] NewArray = Enumerable.Range(1, 8).ToArray();
+{
+    for (int i = 0; i < NewArray.Length; i++)
+    {
+        Console.WriteLine(NewArray[i]);
+    }
+}
+Console.ReadLine();
+/*
